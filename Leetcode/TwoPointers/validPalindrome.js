70f81b776c1943877eae2a86501c1c4587728a54
AndreyNovaes <testederealismo@gmail.com>
2022-09-07 09:34:26 -0300

    feat: add twoPointers algo validPalindrome

--- Leetcode/TwoPointers/validPalindrome.js
+++ Leetcode/TwoPointers/validPalindrome.js
@@ -10,3 +10,22 @@
 // Output: true
 // Explanation: s is an empty string "" after removing non-alphanumeric characters.
 // Since an empty string reads the same forward and backward, it is a palindrome.
+
+function ispalindrome(s) {
+  const sAlphaNumLowerCase = s.replace(/[^0-9a-z]/gi, '').toLowerCase();
+  const lastIndex = sAlphaNumLowerCase.length - 1;
+  let reversedSAlphaNumLowerCase = '';
+  for (let i = lastIndex; i >= 0; i -= 1) {
+    reversedSAlphaNumLowerCase += sAlphaNumLowerCase[i];
+  }
+  return reversedSAlphaNumLowerCase === sAlphaNumLowerCase;
+}
+
+// results for this isPalindrome:
+// https://leetcode.com/submissions/detail/793844064/
+// advantages: good speed and ok memory usage
+// disavantages: bad readability, could be better in either ways, runtime and memory usage
+// Runtime: 102 ms, faster than 64.32% of JavaScript online submissions for Valid Palindrome.
+// Memory Usage: 46.7 MB, less than 36.94% of JavaScript online submissions for Valid Palindrome.
+
+module.exports = ispalindrome;
--- tests/TwoPointers/validPalindrome.test.js
+++ tests/TwoPointers/validPalindrome.test.js
@@ -1,3 +1,10 @@
+const validPalindrome = require('../../Leetcode/TwoPointers/validPalindrome');
+
 test('validPalindrome test', () => {
-  expect(true).toEqual(true);
+  const case1 = "A man, a plan, a canal: Panama"; // true
+  const case2 = "race a car" // false
+  const case3 = " " // true
+  expect(validPalindrome(case1)).toEqual(true);
+  expect(validPalindrome(case2)).toEqual(false)
+  expect(validPalindrome(case3)).toEqual(true)
 });
